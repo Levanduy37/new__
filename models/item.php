8f10eb503fadde101114b999b7f54f10676d3b78
levanduy37 <chuotdn.123@gmail.com>
2024-10-31 16:40:20 +0700

    tuan7

---
+++ models/item.php
@@ -0,0 +1,10 @@
+<?php
+class item extends Db{
+    public function getallitems(){
+        $sql = self::$connection->prepare("SELECT * FROM `items`");
+        $sql->execute();
+        $items = array();
+        $items = $sql->get_result()->fetch_all(MYSQLI_ASSOC);
+        return $items;
+    }
+}
